d53abba3f91e0f3db0ab855f44c888fc31243ae7
enesi sefa <enesisefa380@gmail.com>
2022-06-13 18:04:29 +0200

    finished first person salary

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //person 1
 const grossSalary1 = 500
 let pensionContribution1 = grossSalary1 * 0.05
-let taxableAmount1 = grossSalary1 - pensionContribution1
+let taxedSalary1 = grossSalary1 - pensionContribution1
+const taxUnder250EUR = 6.80
+const taxUnder450EUR = 16
+let taxOver450 = taxedSalary1 - 450
+let taxOver450percentage = taxOver450 * 0.1
+let totaltax = taxUnder250EUR + taxUnder450EUR + taxOver450percentage
+let totalAmount1 = taxedSalary1 - totaltax
 
-let taxedSalary1 = taxableAmount1 * 0.1 
-let totalAmount1 = grossSalary1 - taxedSalary1
-
-console.log("Total amount is :" + totalAmount1 + "Euro")
+console.log("Total amount is :" + totalAmount1 + " EUR")
 
 
 //person 2
@@ -15,4 +18,4 @@ let pensionContribution2 = grossSalary2 * 0.05
 let taxableAmount2 = grossSalary2 - pensionContribution2 
 
 
-console.log("Total amount is :" + taxableAmount2 + "Euro")
+console.log("Total amount is :" + taxableAmount2 + "EUR")
